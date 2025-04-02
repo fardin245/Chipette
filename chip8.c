84f4af2bc14408cd045a28226a7324d41955f18c
HotAndCold <130136057+fardin245@users.noreply.github.com>
2025-04-02 22:31:17 +0600

    Update chip8.c
    Updated status messages.

--- chip8.c
+++ chip8.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
-#include <SDL2/SDL.h>
+#include "include/SDL2/SDL.h"
 
 typedef struct {
     SDL_Window *window;
@@ -27,7 +27,7 @@ typedef struct {
     uint32_t window_scale;  // Window size scaling
     uint32_t emulation_rate; // number of instructions to read per second
     uint8_t memory[4096];   // Chip-8 ram of 4KB (4096 bytes)
-    bool display[64 * 41];  // Display size including the status bar 64 * (32 + 9)
+    bool display[64 * 32];  // Display size 64 * 32
     uint8_t V[16];          // Registers
     uint16_t I;             // Index Pointer
     uint16_t PC;            // Program Counter
@@ -48,26 +48,16 @@ bool initialize_chip8(chip8_t *chip8, const char rom_name[]) {
     chip8->window_height = 32;
     chip8->window_scale = 12;
     chip8->emulation_rate = 600;
-    for (int i = 0; i < 4096; i++) {
-        chip8->memory[i] = 0;
-    }
-    for (int i = 0; i < sizeof chip8->display; i++) {
-        chip8->display[i] = false;
-    }
-    for (int i = 0; i < 16; i++) {
-        chip8->V[i] = 0;
-    }
+    memset(chip8->memory, 0, sizeof(chip8->memory));
+    memset(chip8->display, 0, sizeof(chip8->display));
+    memset(chip8->V, 0, sizeof(chip8->V));
+    memset(chip8->stack, 0, sizeof(chip8->stack));
+    memset(chip8->keypad, 0, sizeof(chip8->keypad));
     chip8->I = 0;
     chip8->PC = 0x200;
-    for (int i = 0; i < 16; i++) {
-        chip8->stack[i] = 0;
-    }
     chip8->SP = &chip8->stack[0];
     chip8->delay_timer = 0;
     chip8->sound_timer = 0;
-    for (int i = 0; i < 16; i++) {
-        chip8->keypad[i] = 0;
-    }
     chip8->state = 1;
     chip8->debug_state = 0;
     chip8->mode = 0;
@@ -89,9 +79,7 @@ bool initialize_chip8(chip8_t *chip8, const char rom_name[]) {
         0xF0, 0x80, 0xF0, 0x80, 0xF0,   // E
         0xF0, 0x80, 0xF0, 0x80, 0x80    // F
     };
-    for (int i = 0; i < 80; i++) {
-        chip8->memory[i] = font[i];
-    }
+    memcpy(chip8->memory, font, sizeof(font));
     // Read rom file
     FILE *rom = fopen(rom_name, "rb");
     fseek(rom, 0, SEEK_END);
@@ -124,7 +112,7 @@ void initialize_sdl(sdl_t *sdl, chip8_t *chip8) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
         printf("SDL Initialization Error: %s\n", SDL_GetError());
     }
-    sdl->window = SDL_CreateWindow("Chipette", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, chip8->window_width * chip8->window_scale, (chip8->window_height + 9) * chip8->window_scale, 0);
+    sdl->window = SDL_CreateWindow("Chipette", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, chip8->window_width * chip8->window_scale, chip8->window_height * chip8->window_scale, 0);
     sdl->renderer = SDL_CreateRenderer(sdl->window, -1, SDL_RENDERER_ACCELERATED);
     sdl->want = (SDL_AudioSpec) {.freq = 44100, .format = AUDIO_S16LSB, .channels = 1, .samples = 512, .callback = audio_callback};
     sdl->device = SDL_OpenAudioDevice(NULL, 0, &sdl->want, &sdl->have, 0);
@@ -153,103 +141,8 @@ void clear_screen(sdl_t *sdl) {
 // Update the screen
 void update_screen(sdl_t *sdl, chip8_t *chip8) {
     SDL_Rect pixel = {.x = 0, .y = 0, .w = chip8->window_scale, .h = chip8->window_scale};
-    // status bar
-    for (uint32_t i = (64 * 33); i < (64 * 34); i++) {
-        chip8->display[i] = true;
-        chip8->display[i + (64 * 7)] = true;
-    }
-    // Status Bar setup to display mode and state information
-    uint32_t status_box[24] = {2176, 2203, 2213, 2239, 2240, 2267, 2277, 2303, 2304, 2331, 2341, 2367, 2368, 2395, 2405, 2431, 2432, 2459, 2469, 2495, 2496, 2523, 2533, 2559};
-    uint32_t default_mode[52] = {2242, 2243, 2246, 2247, 2248, 2250, 2251, 2252, 2254, 2257, 2259, 2261, 2263, 2264, 2265, 2306, 2308, 2310, 2311, 2314, 2317, 2319, 2321, 2323, 2325, 2328, 2370, 2372, 2374, 2378, 2379, 2381, 2382, 2383, 2385, 2387, 2389, 2392, 2434, 2435, 2438, 2439, 2440, 2442, 2445, 2447, 2449, 2450, 2451, 2453, 2454, 2456};
-    uint32_t debug_mode[45] = {2244, 2245, 2248, 2249, 2250, 2252, 2256, 2258, 2260, 2261, 2262, 2308, 2310, 2312, 2313, 2316, 2317, 2318, 2320, 2322, 2324, 2372, 2374, 2376, 2380, 2382, 2384, 2386, 2388, 2390, 2391, 2436, 2437, 2440, 2441, 2442, 2444, 2445, 2446, 2448, 2449, 2450, 2452, 2453, 2455};
-    uint32_t ch_mode[17] = {2269, 2270, 2271, 2273, 2275, 2333, 2337, 2339, 2397, 2401, 2402, 2403, 2461, 2462, 2463, 2465, 2467};
-    uint32_t sc_mode[15] = {2270, 2271, 2273, 2274, 2275, 2333, 2334, 2337, 2399, 2401, 2461, 2462, 2465, 2466, 2467};
-    uint32_t xo_mode[17] = {2269, 2271, 2273, 2274, 2275, 2334, 2337, 2339, 2397, 2399, 2401, 2403, 2461, 2463, 2465, 2466, 2467};
-    uint32_t active_state[44] = {2281, 2284, 2285, 2286, 2288, 2289, 2290, 2292, 2294, 2296, 2298, 2299, 2300, 2344, 2346, 2348, 2353, 2356, 2358, 2360, 2362, 2363, 2408, 2409, 2410, 2412, 2417, 2420, 2422, 2424, 2426, 2472, 2474, 2476, 2477, 2478, 2481, 2484, 2486, 2487, 2488, 2490, 2491, 2492};
-    uint32_t paused_state[52] = {2279, 2280, 2281, 2284, 2287, 2289, 2291, 2292, 2293, 2295, 2296, 2297, 2299, 2300, 2343, 2345, 2347, 2349, 2351, 2353, 2355, 2359, 2360, 2363, 2365, 2407, 2408, 2409, 2411, 2412, 2413, 2415, 2417, 2420, 2421, 2423, 2427, 2429, 2471, 2475, 2477, 2479, 2480, 2481, 2483, 2484, 2485, 2487, 2488, 2489, 2491, 2492};
-    for (int i = 0; i < 24; i++) {
-        chip8->display[status_box[i]] = true;
-    }
-    switch (chip8->mode) {
-        case 0: {
-            for (int i = 0; i < 15; i++) {
-                chip8->display[sc_mode[i]] = false;
-            }
-            for (int i = 0; i < 17; i++) {
-                chip8->display[xo_mode[i]] = false;
-            }
-            for (int i = 0; i < 17; i++) {
-                chip8->display[ch_mode[i]] = true;
-            }
-            break;
-        }
-        case 1: {
-            for (int i = 0; i < 17; i++) {
-                chip8->display[ch_mode[i]] = false;
-            }
-            for (int i = 0; i < 17; i++) {
-                chip8->display[xo_mode[i]] = false;
-            }
-            for (int i = 0; i < 15; i++) {
-                chip8->display[sc_mode[i]] = true;
-            }
-            break;
-        }
-        case 2: {
-            for (int i = 0; i < 15; i++) {
-                chip8->display[sc_mode[i]] = false;
-            }
-            for (int i = 0; i < 17; i++) {
-                chip8->display[ch_mode[i]] = false;
-            }
-            for (int i = 0; i < 17; i++) {
-                chip8->display[xo_mode[i]] = true;
-            }
-            break;
-        }
-    }
-    switch (chip8->debug_state) {
-        case 0: {
-            for (int i = 0; i < 45; i++) {
-                chip8->display[debug_mode[i]] = false;
-            }
-            for (int i = 0; i < 52; i++) {
-                chip8->display[default_mode[i]] = true;
-            }
-            break;
-        }
-        case 1: {
-            for (int i = 0; i < 52; i++) {
-                chip8->display[default_mode[i]] = false;
-            }
-            for (int i = 0; i < 45; i++) {
-                chip8->display[debug_mode[i]] = true;
-            }
-            break;
-        }
-    }
-    switch (chip8->state) {
-        case 1: {
-            for (int i = 0; i < 52; i++) {
-                chip8->display[paused_state[i]] = false;
-            }
-            for (int i = 0; i < 44; i++) {
-                chip8->display[active_state[i]] = true;
-            }
-            break;
-        }
-        case 2: {
-            for (int i = 0; i < 44; i++) {
-                chip8->display[active_state[i]] = false;
-            }
-            for (int i = 0; i < 52; i++) {
-                chip8->display[paused_state[i]] = true;
-            }
-            break;
-        }
-    }
     // Draw the pixels on the display
-    for (uint32_t i = 0; i < chip8->window_width * 41; i++) {
+    for (uint32_t i = 0; i < chip8->window_width * 32; i++) {
         pixel.x = (i % chip8->window_width) * pixel.w;
         pixel.y = (i / chip8->window_width) * pixel.h;
         if (chip8->display[i]) {
@@ -299,9 +192,11 @@ void handle_input(chip8_t *chip8, const char rom_name[]) {
                     else if (event.key.keysym.scancode == SDL_SCANCODE_P) {                 // P to Pause/Unpause
                         if (chip8->state == 1) {
                             chip8->state = 2;
+                            printf("PAUSED\n");
                         }
                         else if (chip8->state == 2) {
                             chip8->state = 1;
+                            printf("UNPAUSED\n");
                         }
                         break;
                     }
@@ -313,10 +208,12 @@ void handle_input(chip8_t *chip8, const char rom_name[]) {
                         if (chip8->debug_state == 0) {
                             chip8->debug_state = 1;
                             chip8->emulation_rate = 1;
+                            printf("DEBUG MODE ACTIVATED\n");
                         }
                         else if (chip8->debug_state == 1) {
                             chip8->debug_state = 0;
                             chip8->emulation_rate = 600;
+                            printf("DEBUG MODE DEACTIVATED\n");
                         }
                         break;
                     }
@@ -324,12 +221,15 @@ void handle_input(chip8_t *chip8, const char rom_name[]) {
                         uint8_t chip_mode = chip8->mode;
                         if (chip_mode == 0) {
                             chip8->mode = 1;
+                            printf("CHIP MODE: SUPERCHIP\n");
                         }
                         else if (chip_mode == 1) {
                             chip8->mode = 2;
+                            printf("CHIP MODE: XO-CHIP\n");
                         }
                         else if (chip_mode == 2) {
                             chip8->mode = 0;
+                            printf("CHIP MODE: CHIP-8\n");
                         }
                         break;
                     }
@@ -354,7 +254,6 @@ void emulate_instruction(chip8_t *chip8, instruction_t *instruction) {
     if (chip8->debug_state) {
         printf("The current instruction is at Address: 0x%04X with opcode: 0x%04X\n", chip8->PC-2, instruction->opcode);
     }
-    
     chip8->PC += 2;
     instruction->NNN = instruction->opcode & 0x0FFF;                                        // Mask upper 4 bits
     instruction->NN = instruction->opcode & 0x00FF;                                         // Mask upper 8 bits
@@ -576,8 +475,8 @@ int main(int argc, char **argv) {
     const char *rom_name = argv[1];                                                         // Take input for rom name
     clock_t start_time, end_time;
     chip8_t chip8;
-    initialize_chip8(&chip8, rom_name);
     sdl_t sdl;
+    initialize_chip8(&chip8, rom_name);
     initialize_sdl(&sdl, &chip8);
     clear_screen(&sdl);
     srand(time(NULL));
@@ -592,11 +491,10 @@ int main(int argc, char **argv) {
         for (uint32_t i = 0; i < chip8.emulation_rate; i++) {                               // Emulate upto 600 instructions in a row or until the display needs a refresh
             emulate_instruction(&chip8, &instruction);
             if (instruction.opcode >> 12 == (0xD | 0x0)) {
-                
                 break;
             }
         }
-        update_screen(&sdl, &chip8);                                                // Update the screen when 0xDXYN or 0x00E0 instructions are encountered
+        update_screen(&sdl, &chip8);                                                        // Update the screen when 0xDXYN or 0x00E0 instructions are encountered
         chip8.draw = false;
         end_time = clock();
         if (chip8.debug_state == 0) {
